065822b607cbe7c2edccf01812f4c6588e1855fe
Rithick <rithick0907@gmail.com>
2021-07-18 12:59:01 +0530

    Page Transition Animation added with React-Transition-Group

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import Routes from "./Routes";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
-  return <Routes />;
+  const location = useLocation();
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="page" timeout={1000}>
+        <Routes />
+      </CSSTransition>
+    </TransitionGroup>
+  );
 };
 
 export default App;
